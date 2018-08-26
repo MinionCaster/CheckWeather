357325ceb5c07efef93e3a7ead7e552f3676de90
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-08-26 13:33:12 +0200

    feather icon for wind

--- src/components/WeatherResult/WeatherResult.js
+++ src/components/WeatherResult/WeatherResult.js
@@ -3,6 +3,11 @@ import React from 'react';
 import Classes from './WeatherResult.css';
 import Aux from '../../hoc/Aux';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFeatherAlt } from '@fortawesome/free-solid-svg-icons';
+library.add(faFeatherAlt)
+
 const WeatherResult = props => {
     return (
         <Aux>
@@ -12,7 +17,7 @@ const WeatherResult = props => {
                 <h3>{props.weather}<span>{props.pressure}hPa <span className={Classes.Dot}>• </span>humidity {props.humidity}%</span></h3>
                 <h1>{props.temperature}°</h1>
                 <h3 className={Classes.MarginSmall}>{props.description}</h3>
-                <h3 className={Classes.Marginsmall}>{props.wind}mph</h3>
+                <h3>{props.wind}mph<span><FontAwesomeIcon icon="feather-alt" /></span></h3>
                 <h3>Weather time: {props.date}</h3>
             </div>
         </Aux>
